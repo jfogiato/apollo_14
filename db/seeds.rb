511383e16ad1783f77b646ed87c52b82398eceef
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-20 14:28:23 -0400

    Complete user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Mission.delete_all
+AstronautMission.delete_all
+Astronaut.delete_all
+
+missions = [
+  {title: "Apollo 13", time_in_space: 14},
+  {title: "Capricorn 4", time_in_space: 5},
+  {title: "Gemini 7", time_in_space: 12},
+  {title: "Mercury 6", time_in_space: 25}
+]
+
+missions.each do |mission|
+  Mission.create!(mission)
+end
+
+puts "Created #{Mission.count} missions"
+
+astronauts = [
+  {name: "Neil Armstrong", age: 37, job: "Commander"},
+  {name: "Buzz Aldrin", age: 32, job: "Commander"},
+  {name: "Alan Shepard", age: 35, job: "Commander"},
+  {name: "John Glenn", age: 40, job: "Commander"}
+]
+
+astronauts.each do |astronaut|
+  Astronaut.create!(astronaut)
+end
+
+puts "Created #{Astronaut.count} astronauts"
+
+astronaut_missions = [
+  {astronaut_id: Astronaut.first.id, mission_id: Mission.first.id},
+  {astronaut_id: Astronaut.first.id, mission_id: Mission.last.id},
+  {astronaut_id: Astronaut.second.id, mission_id: Mission.first.id},
+  {astronaut_id: Astronaut.second.id, mission_id: Mission.last.id},
+  {astronaut_id: Astronaut.third.id, mission_id: Mission.first.id},
+  {astronaut_id: Astronaut.third.id, mission_id: Mission.last.id},
+  {astronaut_id: Astronaut.fourth.id, mission_id: Mission.first.id},
+  {astronaut_id: Astronaut.fourth.id, mission_id: Mission.last.id}
+]
+
+astronaut_missions.each do |astronaut_mission|
+  AstronautMission.create!(astronaut_mission)
+end
+
+puts "Created #{AstronautMission.count} astronaut_missions"
